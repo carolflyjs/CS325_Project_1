21f7c0b83263090ecea89a5c2de8b9c21571970b
carolflyjs <caroljin.js@gmail.com>
2016-10-16 18:16:05 -0700

    completed

---
+++ CS325-Project-1/CS325-Project-1/README.md
@@ -0,0 +1,6 @@
+You can use the provided makefile.
+
+If you prefer to compile it yourself, follow the instructions below:
+1. be sure to use version c++11. g++ -std=c++11
+2. All .cpp files listed here below should be compiled.
+main.cpp operator.cpp 1_num.cpp 2_betterNum.cpp 3_DandC.cpp 4_linear.cpp
--- CS325-Project-1/CS325-Project-1/main.cpp
+++ CS325-Project-1/CS325-Project-1/main.cpp
@@ -4,6 +4,8 @@
 #include <sstream>
 #include <fstream>
 #include <cassert>
+#include <cstdlib>
+#include <ctime>
 
 // print an array to output file
 void printArray(int* array, int length, std::ofstream* output) {
@@ -49,7 +51,6 @@ bool testAlgorithm(int* array, int length) {
 }
 
 int main() {
-
     /* open input & output file */
     std::ifstream inputFile;
     inputFile.open("MSS_Problems.txt");
@@ -117,7 +118,139 @@ int main() {
         length = 0;
         strIndex = 0;
     }
+
+    inputFile.close();
+    outputFile.close();
+
+
+    /* ************************************************************************************
+    Experimental analysis. 
+    For the experimental analysis you will plot running times as a function of input size.  
+    Every programming language should provide access to a clock (not necessarily in seconds).  
+    Run each of your algorithms on various input sizes for example  n = 100 - 900 or 1000 - 10000. 
+    If your algorithm runs very fast and has a time of 0 you will need to increase the sizes of n.  
+    Likewise if the algorithm runs for over an hour you will need to decrease your input size.  
+    Each algorithm may require different sizes of n and you will need at least 10 different 
+    input sizes for each algorithm.  
+    ***************************************************************************************/
+    // test first algorithm with array size from 100 to 900
+   
+   /*
+	srand(time(0));
+    int * expArray;
+    clock_t current;
+    int nTicks;
+
+    outputFile.open("Exp_tracker.txt");
+    if (!outputFile)
+    {
+        std::cout << "Error opening output File\n";
+        return -1;
+    }
+
+    const int testSize = 20;
+    const int test1Range = 100;
+    const int test2Range = 1000;
+    const int test3Range = 1000000;
+    const int test4Range = 100000000;
+
     
+    outputFile << "Algo 1\n";
+    std::cout << "Algo 1\n";
+    for (int i = 1; i < testSize; i++) {
+        expArray = new int[i * test1Range];
+        // generate the array for testing
+        for (int j = 0; j < i * test1Range; j++) {
+            expArray[j] = rand() % 500 - 1000;
+        }
+        outputFile << i * test1Range << " ";
+        std::cout << i * test1Range << " ";
+        for (int j = 0; j < 10; j++) {
+            current = clock();
+            maxSeqEnum(expArray, i * test1Range);
+            nTicks = clock() - current;
+            outputFile << nTicks << " ";
+            std::cout << nTicks << " ";
+        }
+        outputFile << "\n";
+        std::cout << "\n";
+
+        delete []expArray;
+    }
+
+    outputFile << "Algo 2\n";
+    std::cout << "Algo 2\n";
+    for (int i = 1; i < testSize; i++) {
+        expArray = new int[i * test2Range];
+        // generate the array for testing
+        for (int j = 0; j < i * test2Range; j++) {
+            expArray[j] = rand() % 500 - 1000;
+        }
+        outputFile << i * test2Range << " ";
+        std::cout << i * test2Range << " ";
+        for (int j = 0; j < 10; j++) {
+            current = clock();
+            maxSeqBetterEnum(expArray, i * test2Range);
+            nTicks = clock() - current;
+            outputFile << nTicks << " ";
+            std::cout << nTicks << " ";
+        }
+        outputFile << "\n";
+        std::cout << "\n";
+
+        delete[]expArray;
+    }
+   
+    outputFile << "Algo 3\n";
+    std::cout << "Algo 3\n";
+    for (int i = 1; i < testSize; i++) {
+        expArray = new int[i * test3Range];
+        // generate the array for testing
+        for (int j = 0; j < i * test3Range; j++) {
+            expArray[j] = rand() % 500 - 1000;
+        }
+        outputFile << i * test3Range << " ";
+        std::cout << i * test3Range << " ";
+        for (int j = 0; j < 10; j++) {
+            current = clock();
+            maxSeqDandC(expArray, i * test3Range);
+            nTicks = clock() - current;
+            outputFile << nTicks << " ";
+            std::cout << nTicks << " ";
+        }
+        outputFile << "\n";
+        std::cout << "\n";
+
+        delete[]expArray;
+    }
+   
+    outputFile << "Algo 4\n";
+    std::cout << "Algo 4\n";
+
+    for (int i = 1; i < testSize; i++) {
+	    expArray = new int[i * test4Range];
+        // generate the array for testing
+        for (int j = 0; j < i * test4Range; j++) {
+            expArray[j] = rand() % 500 - 1000;
+        }
+        outputFile << i * test4Range << " ";
+        std::cout << i * test4Range << " ";
+        for (int j = 0; j < 10; j++) {
+            current = clock();
+            maxSeqLinear(expArray, i * test4Range);
+            nTicks = clock() - current;
+            outputFile << nTicks << " ";
+            std::cout << nTicks << " ";
+        }
+        outputFile << "\n";
+        std::cout << "\n";
+
+        delete[]expArray;
+    }
+    
+    outputFile.close(); 
+	*/
+
 
     return 0;
 }
